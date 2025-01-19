bba887dacf69be2139c853e9663f1b5f01ad2846
Polawat Huang <polawathuang@gmail.com>
2025-01-19 21:01:56 +0700

    Fix bug on layout

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import { Kanit } from "next/font/google";
 import "./globals.css";
+import HeaderComponent from "@/components/HeaderCompnent";
 
 const kanit = Kanit({
   variable: "--font-kanit",
   subsets: ["latin"],
   weight: ["100","200","300","400","500","600","700"]
 });
+
 export const metadata = {
   title: "MANGA LOVER - อ่านมังงะแปลไทย อัปเดตไวที่สุด!",
   description: "MANGA LOVER คือเว็บไซต์อ่านการ์ตูนออนไลน์ที่ออกแบบมาให้ใช้งานง่าย สะดวก รวดเร็ว พร้อมอัปเดตมังงะใหม่ทุกวัน! พบกับมังงะหลากหลายแนว ทั้งแอ็กชัน โรแมนซ์ แฟนตาซี และอีกมากมาย ทุกเรื่องแปลไทยให้อ่านฟรี คุณภาพคมชัด โหลดไว ไม่พลาดทุกตอนสำคัญ!",
@@ -17,6 +19,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${kanit.variable} antialiased`}
       >
+        <HeaderComponent />
         {children}
       </body>
     </html>
