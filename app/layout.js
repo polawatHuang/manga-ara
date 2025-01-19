0f438d5ac655c5cf3a5ebcf270072421f611b9e9
Polawat Huang <polawathuang@gmail.com>
2025-01-19 20:58:47 +0700

    Fix bug on layout

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Kanit } from "next/font/google";
 import "./globals.css";
-import HeaderComponent from "@/components/HeaderCompnent";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const kanit = Kanit({
+  variable: "--font-kanit",
   subsets: ["latin"],
+  weight: ["100","200","300","400","500","600","700"]
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "MANGA LOVER - อ่านมังงะแปลไทย อัปเดตไวที่สุด!",
   description: "MANGA LOVER คือเว็บไซต์อ่านการ์ตูนออนไลน์ที่ออกแบบมาให้ใช้งานง่าย สะดวก รวดเร็ว พร้อมอัปเดตมังงะใหม่ทุกวัน! พบกับมังงะหลากหลายแนว ทั้งแอ็กชัน โรแมนซ์ แฟนตาซี และอีกมากมาย ทุกเรื่องแปลไทยให้อ่านฟรี คุณภาพคมชัด โหลดไว ไม่พลาดทุกตอนสำคัญ!",
@@ -21,9 +15,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${kanit.variable} antialiased`}
       >
-         <HeaderComponent />
         {children}
       </body>
     </html>
