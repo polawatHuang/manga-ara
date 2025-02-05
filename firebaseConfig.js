4486f2ff55e7cdf9d7d4df0558d399fa9965680e
Polawat Huang <polawathuang@gmail.com>
2025-02-05 20:02:17 +0700

    Fix bug on firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: "AIzaSyBiSpu4PdOrdUGCEDRhSNHpMyft8KKz9rM",
+  authDomain: "manga-ara.firebaseapp.com",
+  projectId: "manga-ara",
+  storageBucket: "manga-ara.firebasestorage.app",
+  messagingSenderId: "879631164216",
+  appId: "1:879631164216:web:70450e69e2bcbb474557cd"
 };
 
 // Initialize Firebase
