6e4a649a681aa5b89362c1db048a590cbf0aa5d7
Polawat Huang <polawat_hua@tdem.toyota-asia.com>
2025-12-10 11:56:58 +0700

    Fix bug on production

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,10 @@ const nextConfig = {
         {
           protocol: 'https',
           hostname: "mangaara.*",
+        },
+        {
+          protocol: 'https',
+          hostname: "manga.cipacmeeting.com",
         }
       ],
     },
